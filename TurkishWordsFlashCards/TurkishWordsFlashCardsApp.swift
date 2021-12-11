aea96b087be33f488fa04db759ac42f1ade3fd3b
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-12-10 22:34:05 -0500

    Initial Commit

---
+++ TurkishWordsFlashCards/TurkishWordsFlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurkishWordsFlashCardsApp.swift
+//  TurkishWordsFlashCards
+//
+//  Created by Berk Bozkurt on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct TurkishWordsFlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
